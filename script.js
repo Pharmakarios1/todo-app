966cfde872430ca4952d19fe48ba6a1e6ea358bc
pharmakarios1 <akhigbeblessed@gmail.com>
2024-05-17 15:17:46 +0100

    counter function implemented

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const todoInput = document.querySelector("#todo-input");
 const todoSectionOne = document.querySelector(".section-one");
 const todoWrapper = document.querySelector(".todo-wrapper");
+const totalCount = document.querySelector("#todo-count");
+
+totalCount.textContent = 0;
 
 function HandleAdd() {
-  if (todoInput.value == "") {
+  if (!todoInput.value) {
     alert("Please add your todo(s)!");
   } else {
     const todoList = document.createElement("ul");
     const buttons = document.createElement("div");
 
     buttons.className = "marks";
-    buttons.innerHTML = ` <img src="./img/check (2).png" alt="checkmark" />
-    <img src="./img/delete.png" alt="delete" />`;
+    buttons.innerHTML = `<img src="./img/check (2).png" alt="checkmark" onclick="HandleAdd"/>
+    <img src="./img/delete.png" alt="delete" onclick="HandleDelete()" />`;
 
     todoList.classList.add("todo-item__wrapper");
     let li = document.createElement("li");
@@ -19,6 +22,16 @@ function HandleAdd() {
     li.innerText = todoInput.value;
 
     todoWrapper.appendChild(todoList).appendChild(li).appendChild(buttons);
+
+    totalCount.textContent = todoWrapper.childElementCount;
   }
+
   todoInput.value = "";
 }
+
+// totalCount.textContent = "6";
+function HandleDelete() {
+  todoSectionOne.removeChild();
+}
+
+// function HandleAdd() {}
